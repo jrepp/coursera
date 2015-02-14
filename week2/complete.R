a10199f8a3874dff9f819fe41244de7325a649c0
Jacob Repp <jacobrepp@gmail.com>
2015-02-14 10:22:50 -0800

    adding week 2 programs

---
+++ week2/complete.R
@@ -0,0 +1,14 @@
+complete <- function(directory, file.ids=1:332) {
+  nobs = c()
+  id = c()
+  for (i in file.ids) {
+    ## read in the observations
+    file = file.path(directory, paste(sprintf("%03d", i), ".csv", sep=""))
+    df = read.csv(file)
+    complete = df[complete.cases(df),]
+    nobs = c(nobs, length(complete[[1]]))
+    id = c(id, i)
+  }
+  
+  cbind(id, nobs)
+}
--- week2/pollutantmean.R
+++ week2/pollutantmean.R
@@ -13,3 +13,21 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
 	}
 	mean(total_samples)
 }
+
+pollutantmean2 <- function(directory, pollutant, id = 1:332) {
+  total = 0
+  count = 0
+  for (i in id) {
+    ## read in the observations
+    file = file.path(directory, paste(sprintf("%03d", i), ".csv", sep=""))
+    df = read.csv(file)
+    
+    ## filter the NA values and select samples
+    samples = df[!is.na(df[[pollutant]]),][[pollutant]]
+    
+    ## concat samples
+    total = total + sum(samples)
+    count = count + length(samples)
+  }
+  total / count
+}
